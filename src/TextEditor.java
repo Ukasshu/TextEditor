28e2348146a9dc3bb66eef2bd9dbb82c73db5b6b
Ukasshu <mielczareklukasz96@gmail.com>
2016-11-27 18:08:09 +0100

    Dodane wklejanie, wycinanie, kopiowanie tekstu, menu dot. czcionki (oprogramowac je), blokowanie elementow dot. czcionki przy PlainTextMode'zie, skonczone zapisywanie plikow (dodac "czy chcesz zapisac zmiany"),

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.text.rtf.RTFEditorKit;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -24,45 +28,66 @@ public class TextEditor extends JFrame {
     private JButton copyButton;
     private JButton pasteButton;
     private JButton newRTFButton;
+    private JButton boldButton;
+    private JButton italicButton;
+    private JButton underlineButton;
+    private JComboBox fontComboBox;
 
 
     private static int windowsCounter = 0;
     private File currentFile = null;
+    private String mode = null;
 
-    public TextEditor(String type){
+    public TextEditor(String type) {
         setContentPane(mainPanel);
         menuBar = new MenuBarBuilder().menuBar;
         createMenusListeners();
         setJMenuBar(menuBar);
         setTitle("Untitled - TextEditor");
+        System.out.println(type);
+        if(type.equals("text/plain")){
+            mode = "PlainTextMode";
+            EventQueue.invokeLater(new LockFontElements());
+        }
+        else{
+            mode = "RichTextMode";
+            EventQueue.invokeLater(new UnlockFontElements());
+        }
+        setTitle("Untitled - " + mode + " - TextEditor");
+        GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        for(String fontName: GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames()){
+            fontComboBox.addItem(fontName);
+        }
+        fontComboBox.setSelectedItem("Arial");
+        editorPane.setFont(new Font("Arial", Font.BOLD, 20));
+        System.out.println(Font.getFont("Tahoma"));
         windowsCounter++;
         editorPane.setEditorKit(editorPane.getEditorKitForContentType(type));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(args.length);
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         JFrame frame;
-        if(args.length == 0) {
-            frame = new TextEditor("type/plain");
-        }
-        else{
+        if (args.length == 0) {
+            frame = new TextEditor("text/plain");
+        } else {
             frame = new TextEditor(args[0]);
         }
         frame.pack();
         frame.setVisible(true);
-        if(windowsCounter == 0){
+        if (windowsCounter == 0) {
             System.exit(0);
         }
     }
 
-    private void createMenusListeners(){
+    private void createMenusListeners() {
         JMenu menu = menuBar.getMenu(0);
         ActionListener al = new NewActionListener();
         menu.getItem(0).addActionListener(al);
@@ -90,9 +115,11 @@ public class TextEditor extends JFrame {
         al = new PasteActionListener();
         menu.getItem(2).addActionListener(al);
         pasteButton.addActionListener(al);
-        menu.getItem(4).addActionListener(new FormatActionListener());
         menu = menuBar.getMenu(2);
         menu.getItem(0).addActionListener(new AboutActionListener());
+        boldButton.addActionListener(new BoldActionListener());
+        italicButton.addActionListener(new ItalicActionListener());
+        underlineButton.addActionListener(new UnderlineActionListener());
     }
 
     private class MenuBarBuilder {
@@ -116,8 +143,6 @@ public class TextEditor extends JFrame {
             menu.add(new JMenuItem("Cut..."));
             menu.add(new JMenuItem("Copy..."));
             menu.add(new JMenuItem("Paste..."));
-            menu.addSeparator();
-            menu.add(new JMenuItem("Format..."));
             menuBar.add(menu);
             menu = new JMenu("Help");
             menu.setMnemonic(KeyEvent.VK_H);
@@ -126,22 +151,42 @@ public class TextEditor extends JFrame {
         }
     }
 
+    private class LockFontElements implements Runnable {
+        @Override
+        public void run() {
+            boldButton.setEnabled(false);
+            italicButton.setEnabled(false);
+            underlineButton.setEnabled(false);
+            fontComboBox.setEnabled(false);
+        }
+    }
+
+    private class UnlockFontElements implements Runnable {
+        @Override
+        public void run() {
+            boldButton.setEnabled(true);
+            italicButton.setEnabled(true);
+            underlineButton.setEnabled(true);
+            fontComboBox.setEnabled(true);
+        }
+    }
+
     @Override
-    public void dispose(){
+    public void dispose() {
         windowsCounter--;
         super.dispose();
     }
 
-    private class NewActionListener implements ActionListener{
+    private class NewActionListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             String[] args = new String[1];
             args[0] = "text/plain";
             main(args);
         }
     }
 
-    private class NewRTFActionListener implements ActionListener{
+    private class NewRTFActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             String[] args = new String[1];
@@ -150,14 +195,13 @@ public class TextEditor extends JFrame {
         }
     }
 
-    private class OpenActionListener implements ActionListener{
+    private class OpenActionListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
-            try{
+        public void actionPerformed(ActionEvent e) {
+            try {
                 EventQueue.invokeLater(new OpenFile());
-            }
-            catch(Exception ex){
-                    JOptionPane.showMessageDialog(mainPanel, "Cannot open the file");
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(mainPanel, "Cannot open the file");
             }
         }
     }
@@ -165,19 +209,18 @@ public class TextEditor extends JFrame {
     private class OpenFile implements Runnable {
         public void run() {
             JFileChooser fc = new JFileChooser("~");
-            if(fc.showDialog(mainPanel, "Open file") == JFileChooser.APPROVE_OPTION){
+            if (fc.showDialog(mainPanel, "Open file") == JFileChooser.APPROVE_OPTION) {
                 currentFile = fc.getSelectedFile();
-                if(new FileNameExtensionFilter("RTF File", "rtf").accept(currentFile)){
+                if (new FileNameExtensionFilter("RTF File", "rtf").accept(currentFile)) {
                     editorPane.setEditorKit(editorPane.getEditorKitForContentType("text/rtf"));
-                }
-                else{
+                } else {
                     editorPane.setEditorKit(editorPane.getEditorKitForContentType("text/plain"));
                 }
                 try {
                     FileReader fr = new FileReader(currentFile);
                     editorPane.getEditorKit().read(fr, editorPane.getDocument(), 0);
                     fr.close();
-                }catch(Exception e){
+                } catch (Exception e) {
                     JOptionPane.showMessageDialog(mainPanel, "Cannot open this file!");
                 }
                 setTitle(currentFile.getName() + " - TextEditor");
@@ -186,28 +229,26 @@ public class TextEditor extends JFrame {
         }
     }
 
-    private class SaveActionListener implements ActionListener{
+    private class SaveActionListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             //JOptionPane.showMessageDialog(mainPanel, "SAVE");
-            if(currentFile==null){
+            if (currentFile == null) {
                 EventQueue.invokeLater(new SaveAs());
-            }
-            else{
+            } else {
                 EventQueue.invokeLater(new SaveFile());
             }
         }
     }
 
-    private class SaveFile implements Runnable{
-        public void run(){
-            try{
-                if(editorPane.getContentType().equals("text/plain")) {
+    private class SaveFile implements Runnable {
+        public void run() {
+            try {
+                if (editorPane.getContentType().equals("text/plain")) {
                     FileWriter fw = new FileWriter(currentFile);
                     editorPane.getEditorKit().write(fw, editorPane.getDocument(), 0, editorPane.getDocument().getLength());
                     fw.close();
-                }
-                else{
+                } else {
                     FileOutputStream fos = new FileOutputStream(currentFile);
                     BufferedOutputStream bos = new BufferedOutputStream(fos);
                     editorPane.getEditorKit().write(bos, editorPane.getDocument(), 0, editorPane.getDocument().getLength());
@@ -215,67 +256,136 @@ public class TextEditor extends JFrame {
                     fos.close();
                 }
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 JOptionPane.showMessageDialog(mainPanel, "Cannot save file!");
-                e.printStackTrace();
             }
         }
     }
 
-    private class SaveAsActionListener implements ActionListener{
+    private class SaveAsActionListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
-            JFileChooser fc = new JFileChooser("~");
-
+        public void actionPerformed(ActionEvent e) {
+            EventQueue.invokeLater(new SaveAs());
         }
     }
 
-    private class SaveAs implements Runnable{
+    private class SaveAs implements Runnable {
         @Override
         public void run() {
+            JFileChooser fc = new JFileChooser("~");
+            if (fc.showDialog(mainPanel, "Save file") == JFileChooser.APPROVE_OPTION) {
+                currentFile = fc.getSelectedFile();
+                try {
+                    if (editorPane.getContentType().equals("text/rtf")) {
+                        if (!currentFile.getPath().matches("(.*)[.]rtf")) {
+                            currentFile = new File(currentFile.getPath() + ".rtf");
+                        }
+                        FileOutputStream fos = new FileOutputStream(currentFile);
+                        BufferedOutputStream bos = new BufferedOutputStream(fos);
+                        editorPane.getEditorKit().write(bos, editorPane.getDocument(), 0, editorPane.getDocument().getLength());
+                        bos.close();
+                        fos.close();
+                    } else {
+                        FileWriter fw = new FileWriter(currentFile);
+                        editorPane.getEditorKit().write(fw, editorPane.getDocument(), 0, editorPane.getDocument().getLength());
+                        fw.close();
+                    }
+                    setTitle(currentFile.getName() + " - TextEditor");
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(mainPanel, "Cannot save the file!");
+                }
+
+            }
+        }
+    }
+
+    private class ExitActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+        }
+    }
+
+    private class CutActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            EventQueue.invokeLater(new Cut());
+        }
+    }
+
+    private class Cut implements Runnable{
+        @Override
+        public void run() {
+            Clipboard cp = Toolkit.getDefaultToolkit().getSystemClipboard();
+            StringSelection stringSelection = new StringSelection(editorPane.getSelectedText());
+            cp.setContents(stringSelection, null);
+            editorPane.replaceSelection("");
+        }
+    }
+
+    private class CopyActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            EventQueue.invokeLater(new Copy());
+        }
+    }
+
+    private class Copy implements Runnable{
+        @Override
+        public void run() {
+            Clipboard cp = Toolkit.getDefaultToolkit().getSystemClipboard();
+            StringSelection stringSelection = new StringSelection(editorPane.getSelectedText());
+            cp.setContents(stringSelection, null);
+        }
+    }
+
+    private class PasteActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            EventQueue.invokeLater(new Paste());
+        }
+    }
+
+    private class Paste implements Runnable{
+        @Override
+        public void run() {
+            Clipboard cp = Toolkit.getDefaultToolkit().getSystemClipboard();
+            Transferable t = cp.getContents(this);
+            try {
+                editorPane.replaceSelection((String) t.getTransferData(DataFlavor.stringFlavor));
+            }catch(Exception exc){
+                JOptionPane.showMessageDialog(mainPanel, "Pasting error!");
+            }
+        }
+    }
+
+    private class AboutActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(mainPanel, "TextEditor v.1.0\n" +
+                    "Author: Łukasz Mielczarek\n" +
+                    "Github: http://github.com/Ukasshu", "About", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+    private class BoldActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
 
         }
     }
 
-    private class ExitActionListener implements ActionListener{
+    private class ItalicActionListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
-            JOptionPane.showMessageDialog(mainPanel, "EXIT");
+        public void actionPerformed(ActionEvent e) {
+
         }
     }
 
-    private class CutActionListener implements ActionListener{
+    private class UnderlineActionListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
-            JOptionPane.showMessageDialog(mainPanel, "CUT");
-        }
-    }
+        public void actionPerformed(ActionEvent e) {
 
-    private class CopyActionListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e){
-            JOptionPane.showMessageDialog(mainPanel, "COPY");
-        }
-    }
-
-    private class PasteActionListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e){
-            JOptionPane.showMessageDialog(mainPanel, "PASTE");
-        }
-    }
-
-    private class FormatActionListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e){
-            JOptionPane.showMessageDialog(mainPanel, "FORMAT");
-        }
-    }
-
-    private class AboutActionListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e){
-            JOptionPane.showMessageDialog(mainPanel, "ABOUT");
         }
     }
 }
